98c685c09f81fe2bce37097641a688a28fd7ebde
Chadd <chadd_artaud@cox.net>
2022-11-14 10:30:01 -0800

    file structure

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Login from './pages/Login';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+const styles = {
+  portfolio: {
+    height: '1000px'
+  }
+}
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Nav />
+          <Routes>
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+          </Routes>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
